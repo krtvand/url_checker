3f797aeb00ba01c518ebee08b042528a6b7ea8bc
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-03-13 13:35:42 +0000

    include queue

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import xml.etree.ElementTree as ET
 
 
 DUMP = 'dump.xml'
+NUM_CONSUMERS = 10
 
 # Зададим параметры логгирования
 logger = logging.getLogger(__name__)
@@ -24,7 +25,7 @@ def get_data_for_checker():
     - Если нет URL записи, но есть доменное имя, добавляем к общему списку домен
     - IP адреса в текущей версии не проверяются
     """
-    tree = ET.parse('dump_f')
+    tree = ET.parse(DUMP)
     root = tree.getroot()
     data_for_check = []
     for elem_content in root:
@@ -45,30 +46,63 @@ def get_data_for_checker():
         #         data_for_check.add(domain)
     return data_for_check
 
-async def fetch(session, url):
-    try:
-        async with session.get(url) as response:
-            logger.debug('Checking {} status {}'.format(url, response.status))
-            return response.status
-    except Exception as e:
-        logger.debug('Can not open {} : {}'.format(url, e))
-        return -1
+async def fetch(q, session):
+    while True:
+        url = await q.get()
+        if url:
+            try:
+                async with session.get(url, timeout=10) as response:
+                    logger.debug('Checking {} status {}'.format(url, response.status))
+            except Exception as e:
+                logger.debug('Can not open {} : {}'.format(url, e))
+        else:
+            # None is the signal to stop.
+            q.task_done()
+            break
+    print('consumer ending')
+    print('queue size {}'.format(q.qsize()))
 
-async def fetch_all(session, essiourls, loop):
-    done, pending = await asyncio.wait([loop.create_task(fetch(session, url))
-                                  for url in urls])
-    return done
+async def producer(q, urls):
+    print('producer: starting')
+    # Add some numbers to the queue to simulate jobs
+    for url in urls:
+        await q.put(url)
+
+    # Add None entries in the queue
+    # to signal the consumers to exit
+    print('producer: adding stop signals to the queue')
+    for i in range(10):
+        await q.put(None)
+    print('producer: waiting for queue to empty')
+    print('queue size {}'.format(q.qsize()))
+    q.join()
+    print('producer: ending')
+
+async def main(loop):
+    # Create the queue with a fixed size so the producer
+    # will block until the consumers pull some items out.
+    q = asyncio.Queue(maxsize=NUM_CONSUMERS)
+
+    urls = get_data_for_checker()
+    urls = urls[:50]
+    print(len(urls))
+
+    with aiohttp.ClientSession(loop=loop) as session:
+        # Scheduled the consumer tasks.
+        consumers = [
+            loop.create_task(fetch(q, session)) for i in range(NUM_CONSUMERS)
+            ]
+
+        # Schedule the producer task.
+        prod = loop.create_task(producer(q, urls))
+        await asyncio.wait(consumers + [prod])
+    print('ok')
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
 
-    urls = get_data_for_checker()
-    urls = urls[:1000]
-    print(len(urls))
-    with aiohttp.ClientSession(loop=loop) as session:
-        res = loop.run_until_complete(
-            fetch_all(session, urls, loop))
-    print(len(res))
+    try:
+        res = loop.run_until_complete(main(loop))
+    finally:
+        loop.close()
 
-    # for it in htmls:
-    #     print(it.result())
